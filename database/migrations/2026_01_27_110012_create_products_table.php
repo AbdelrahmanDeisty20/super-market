2f449fadfecc36f884ca550f2bb75bf9d2a64f56
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-01-29 11:43:33 +0200

    edit  migrations

--- database/migrations/2026_01_27_110012_create_products_table.php
+++ database/migrations/2026_01_27_110012_create_products_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name_ar');
             $table->string('name_en');
-            $table->te('description_ar');
-            $table->string('description_en');
+            $table->text('description_ar');
+            $table->text('description_en');
             $table->string('image');
             $table->json('images');
             $table->decimal('price', 10, 2);
