b93bcb80fea0c8f38e86e75e3520861b2428d593
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-02-10 11:36:36 +0200

    edit users migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string("phone");
-            $table->enum("user_type",["admin","subAdmin","user"])->default("user");
+            $table->string("")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
