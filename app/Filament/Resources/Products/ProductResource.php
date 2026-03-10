11946cbac35159ffb1b67ff0194ebea31d156aa4
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-03-10 12:00:54 +0200

    feat: add Filament product resource with view page and queued email verification listener.

--- app/Filament/Resources/Products/ProductResource.php
+++ app/Filament/Resources/Products/ProductResource.php
@@ -5,8 +5,10 @@ namespace App\Filament\Resources\Products;
 use App\Filament\Resources\Products\Pages\CreateProduct;
 use App\Filament\Resources\Products\Pages\EditProduct;
 use App\Filament\Resources\Products\Pages\ListProducts;
+use App\Filament\Resources\Products\Pages\ViewProduct;
 use App\Filament\Resources\Products\Schemas\ProductForm;
 use App\Filament\Resources\Products\Tables\ProductsTable;
+use App\Filament\Resources\Products\ProductResource as BaseProductResource;
 use App\Models\Product;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
@@ -61,6 +63,7 @@ class ProductResource extends Resource
         return [
             'index' => ListProducts::route('/'),
             'create' => CreateProduct::route('/create'),
+            'view' => ViewProduct::route('/{record}'),
             'edit' => EditProduct::route('/{record}/edit'),
         ];
     }
